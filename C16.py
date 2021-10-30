55ed56412f9ea5faa0ccdb201e87c109f6352d5d
Koodi-S <93386001+Koodi-S@users.noreply.github.com>
2021-10-30 12:30:01 +0300

    Add files via upload

---
+++ C16.py
@@ -0,0 +1,21 @@
+# -*- coding:UTF-8 -*-
+from bs4 import BeautifulSoup
+import requests
+import time
+import csv
+import re
+
+if __name__ == '__main__':
+    target = 'https://www.storytel.com/fi/fi/categories/16-Nuoret'
+    req = requests.get(url=target)
+    html = req.text
+    div_bf = BeautifulSoup(html, 'html.parser')
+    div = div_bf.find_all('div', class_ = 'gridBookTitle')
+    soup = BeautifulSoup(str(div), 'html.parser')
+    title = []
+    for bookname in soup.find_all('a'):
+        title.append(bookname.string)
+
+    with open('Nuoret.csv','w', newline='', encoding='UTF-8') as file:
+            writer = csv.writer(file)
+            writer.writerows([title])
